40b60d8f109df56550bbc687d07732d1eea35ee7
Elizabeth Klymova <lozaklimovaaadd@gmail.com>
2024-03-16 14:38:11 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import '../src/assets/i18/i18';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/task-pro-2.0">
+      <BrowserRouter basename="/">
         <GlobalStyles />
         <App />
       </BrowserRouter>
